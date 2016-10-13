68d0af2469636ae9e5dfdbf434f616d09047463a
Vadim Shashin <vashashin@avito.ru>
2016-10-13 23:06:48 +0300

    gettext test

--- wda/gettext_test.go
+++ wda/gettext_test.go
@@ -1,19 +1,19 @@
 package wda
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-type FakeGettextRequesterClient struct {}
+type FakeGettextRequesterClient struct{}
 
 func (c *FakeGettextRequesterClient) BundleId() string {
 	return "fake.bundleid"
 }
 
 func (c *FakeGettextRequesterClient) url(uri string) string {
-	fmt.Println("http://fakeurl.fake:80/" + uri)
 	return "http://fakeurl.fake:80/" + uri
 }
 
@@ -26,7 +26,7 @@ func (c *FakeGettextRequesterClient) get(uri string) ([]byte, error) {
 	case "/session/fakesession/element/fakeelementid/text":
 		res = "{\"value\": \"FakeText\", \"status\": 0}"
 	default:
-		res = "{\"error\": 1}"
+		return nil, errors.New("Error doing request")
 	}
 	return []byte(res), nil
 }
@@ -36,11 +36,16 @@ func (c *FakeGettextRequesterClient) post(uri string, data []byte) ([]byte, erro
 
 	switch uri {
 	case "/session/fakesession/element":
-		res = "{\"value\": {\"ELEMENT\": \"fakeelementid\", \"type\": \"faketype\"}, \"status\": 0}"
+		var findResp FindRequest
+		err := json.Unmarshal(data, &findResp)
+		if err == nil && findResp.Value == "findme" {
+			res = "{\"value\": {\"ELEMENT\": \"fakeelementid\", \"type\": \"faketype\"}, \"status\": 0}"
+		} else {
+			return nil, errors.New("Error doing request")
+		}
 	default:
-		res = "{\"error\": 1}"
+		return nil, errors.New("Error doing request")
 	}
-
 	return []byte(res), nil
 }
 
@@ -48,10 +53,18 @@ func TestClient_GetText(t *testing.T) {
 	requester := &FakeGettextRequesterClient{}
 	client := NewBasicClient()
 	client.SetClient(requester)
+
+	// test success
 	findResp, err := client.Find("xui", "findme")
 	assert.NoError(t, err, "Error finding fake element")
 	elementId := findResp.Value.ElementId
 	textResp, err := client.GetText(elementId)
 	assert.NoError(t, err, "Error getting fake element text")
 	assert.Equal(t, "FakeText", textResp.Value, "Text not equals")
+
+	// test error
+	_, err = client.Find("xui", "not_exists_element")
+	assert.Error(t, err, "Element must not be found")
+	_, err = client.GetText("no_element")
+	assert.Error(t, err, "Element text must not be found")
 }
