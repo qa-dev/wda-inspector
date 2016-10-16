0e83eb543341edcad9958c55b40e8b504484a77c
Alexey Makhov <amakhov@avito.ru>
2016-10-16 22:24:20 +0300

    readme and small improvements

---
+++ README.md
@@ -0,0 +1,32 @@
+wda-inspector
+=============
+
+*wda-inspector* is a useful inspector of [qa-dev/WebDriverAgent](https://github.com/qa-dev/WebDriverAgent) (forked from [facebook/WebDriverAgent](https://github.com/facebook/WebDriverAgent))
+written in Go.
+ 
+Build
+-----
+Clone wda-inspector to `$GOPATH` according to [Golang code organization rules](https://golang.org/doc/code.html#Organization)
+
+```
+$ cd $GOPATH/src/github.com/qa-dev/wda-inspector
+$ go build
+```
+
+
+Usage
+-----
+
+```
+$ ./wda-inspector -h=127.0.0.1 -bundleId=com.apple.mobilesafari
+```
+
+Flags:
+```
+-bundleId string
+    	Bundle Id
+-h string
+    	WDA host
+-p string
+    	WDA port. optional (default "8100")
+```
--- main.go
+++ main.go
@@ -13,8 +13,8 @@ import (
 func main() {
 	var iHost, iPort, bundleId string
 	flag.StringVar(&iHost, "h", "", "WDA host")
-	flag.StringVar(&iPort, "p", "8100", "WDA port, 8100 by default")
-	flag.StringVar(&bundleId, "bundleId", "", "Bundle Id, default 'ru.avito.services.dev'")
+	flag.StringVar(&iPort, "p", "8100", "WDA port, optional")
+	flag.StringVar(&bundleId, "bundleId", "", "Bundle Id")
 	flag.Parse()
 	if iHost == "" {
 		log.Fatal("PLease set inspectors host, for example '-h=10.10.10.48'")
@@ -36,10 +36,4 @@ func setHandlers(mux *http.ServeMux, iClient *wda.Client) {
 	mux.Handle("/find", middleware.New(handlers.NewFindHandler(iClient)))
 	mux.Handle("/getText", middleware.New(handlers.NewGetTextHandler(iClient)))
 	mux.Handle("/", middleware.New(handlers.NewIndexPage()))
-}
-
-func fatalErr(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
 }
